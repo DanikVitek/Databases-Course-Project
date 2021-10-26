b0e2a8f4d44ae81c5f27e7cd2a0ddd552af2bd75
Danik Vitek <x3665107@gmail.com>
2021-10-26 21:29:50 +0300

    updated scripts

--- sql scripts/side stuff creation.sql
+++ sql scripts/side stuff creation.sql
@@ -1,27 +1,20 @@
-set @total_users = 0;
-create trigger total_users_increment after insert on `course_project`.`users`
-for each row set @total_users = @total_users + 1;
-create trigger total_users_decrement after insert on `course_project`.`users`
-for each row set @total_users = @total_users - 1;
-
-set @total_plugins = 0;
-create trigger total_plugins_increment after insert on `course_project`.`plugins`
-for each row set @total_plugins = @total_plugins + 1;
-create trigger total_plugins_decrement after insert on `course_project`.`plugins`
-for each row set @total_plugins = @total_plugins - 1;
-
 delimiter //
-create procedure publish_plugin (in pl_title varchar(200), in pl_description text, in pl_category int, in pl_authors json)
+create procedure publish_plugin 
+	(in pl_title varchar(200), in pl_description text, in pl_category smallint, in pl_icon mediumblob, in pl_version varchar(20), in pl_file longblob, in pl_authors json)
 begin
-	insert into `course_project`.`plugins` (title, plugin_description, category_id) values(pl_title, pl_description, pl_category);
-    set @current_pl_id = -1;
-    select id from `course_project`.`plugins` where title = pl_title into @current_pl_id;
-    set @indx = 0;
+	declare current_pl_id bigint;
+	declare indx bigint;
+    declare author_id bigint;
+    
+	insert into `course_project`.`plugins` (title, plugin_description, category_id, icon) values(pl_title, pl_description, pl_category, pl_icon);
+    select id from `course_project`.`plugins` where title = pl_title into current_pl_id;
+    insert into `course_project`.`plugin_versions` (plugin_id, version_title, `file`) values(current_pl_id, pl_version, pl_file);
+    set indx = 0;
 	repeat
-		set @author_id_ = json_extract(pl_authors, concat('$[', @indx, ']'));
-		insert into `course_project`.`plugins_authors` values(@author_id_, @current_pl_id);
-		set @indx = @indx  + 1;
-		until @indx = json_length(pl_authors)
+		set author_id = json_extract(pl_authors, concat('$[', indx, ']'));
+		insert into `course_project`.`plugins_authors` values(author_id, current_pl_id);
+		set indx = indx  + 1;
+		until indx = json_length(pl_authors)
 	end repeat;
 end//
 
@@ -35,30 +28,34 @@ begin
 	delete from `course_project`.`plugins` where id = pl_id;
 end//
 
-create procedure update_plugin (in pl_id bigint, in pl_title varchar(200), in pl_description text, in pl_category int, in pl_authors json)
+create procedure update_plugin (in pl_id bigint, in pl_title varchar(200), in pl_description text, in pl_category int, in pl_icon mediumblob, in pl_authors json)
 begin
+	declare indx bigint;
+    declare author_id bigint;
+    
 	update `course_project`.`plugins`
     set title = pl_title,
 		plugin_description = pl_description,
 		category_id = pl_category,
         plugin_description = pl_description,
-        category_id = pl_category
+        category_id = pl_category,
+        icon = pl_icon
 	where id = pl_id;
     delete from `course_project`.`plugins_authors` where plugin_id = pl_id;
-    set @indx = 0;
+    set indx = 0;
 	repeat
-		set @author_id_ = json_extract(pl_authors, concat('$[', @indx, ']'));
-		insert into `course_project`.`plugins_authors` values(@author_id_, pl_id);
-		set @indx = @indx  + 1;
-		until @indx = json_length(pl_authors)
+		set author_id = json_extract(pl_authors, concat('$[', indx, ']'));
+		insert into `course_project`.`plugins_authors` values(author_id, pl_id);
+		set indx = indx  + 1;
+		until indx = json_length(pl_authors)
 	end repeat;
 end//
 
 create function count_plugins ()
 returns bigint
 begin
-	set @amount = 0;
-	select count(1) from `course_project`.`plugins` into @amount;
-	return @amount;
+	declare amount bigint;
+	select count(1) from `course_project`.`plugins` into amount;
+	return amount;
 end//
 delimiter ;
