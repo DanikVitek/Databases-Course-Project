4993549523adc56b1076b459f1840b23bc8555a4
Danik Vitek <x3665107@gmail.com>
2021-11-14 15:27:52 +0200

    initial commit

---
+++ MCPluginMarketplace/src/main/java/com/danikvitek/MCPluginMarketplace/McPluginMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.danikvitek.MCPluginMarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McPluginMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McPluginMarketplaceApplication.class, args);
+	}
+
+}
---
+++ MCPluginMarketplace/src/test/java/com/danikvitek/MCPluginMarketplace/McPluginMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danikvitek.MCPluginMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McPluginMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
