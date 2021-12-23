caf46140ad8f7ad929ed89fe82164f6259779b02
Danik Vitek <x3665107@gmail.com>
2021-12-24 01:09:44 +0200

    Cleared from odd files

--- sql scripts/side stuff removal.sql
+++
@@ -1,8 +0,0 @@
-drop trigger `course_project`.`total_users_increment`;
-drop trigger `course_project`.`total_users_decrement`;
-drop trigger `course_project`.`total_plugins_increment`;
-drop trigger `course_project`.`total_plugins_decrement`;
-
-drop procedure `course_project`.`publish_plugin`;
-drop procedure `course_project`.`delete_plugin`;
-drop procedure `course_project`.`update_plugin`;
